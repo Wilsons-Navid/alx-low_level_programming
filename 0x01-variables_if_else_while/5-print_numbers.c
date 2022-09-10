5e12373f79ba9c69190e8cf37d58761e483b8b28
Wilsons-Navid <wilson@example.com>
2022-09-10 01:59:26 -0700

    task 5 c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all singledigits in base 10
+ * all your code should be in the main function
+ * Return:0
+ */
+
+int main(void)
+{
+int n;
+while (n<10)
+{
+printf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
