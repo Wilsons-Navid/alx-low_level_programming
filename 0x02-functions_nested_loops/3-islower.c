9abf655a945eca08bd0b4295fc453982afe133ad
Wilsons-Navid <wilson@example.com>
2022-09-14 01:39:07 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,8 @@
 /**
  * _islower - function to check the lower case character
  * @c: is the int that will be use as the argument of the function
- * Return : 0
+ * Return: 0
  */
-
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
