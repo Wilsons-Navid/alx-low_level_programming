6e89eafd4e9f3c5dbbc482c51c0bbd2d1faa0327
Wilsons-Navid <wilson@example.com>
2022-09-14 03:30:44 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function to print the absolute values
+ * @c: argument to be pass in the function
+ * Return: 0
+ */
+int _abs(int c)
+{
+if (c > 0 || c == 0)
+{
+return (c);
+}
+else
+return (c * -1);
+}
