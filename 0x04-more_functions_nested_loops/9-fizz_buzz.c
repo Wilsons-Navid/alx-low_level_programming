7b3eded9b3b56c2ea81d867416d1d7694431b432
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-17 02:48:26 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program to print the reqiured res
+ * Return: return 0
+ */
+
+int main(void)
+{
+int num = 1;
+while (num++ < 100)
+{
+if ((num % 3 == 0) && (num % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if ((num % 3) == 0)
+{
+printf("Fizz ");
+}
+else if ((num % 5) == 0)
+{
+if (num != 100)
+{
+printf("Buzz ");
+}
+else
+{
+printf("Buzz");
+}
+}
+}
+return (0);
+}
