bafe18cbbf1a9b814f0e04f78c23543562f6e955
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-21 13:38:41 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * rot13 - rotate characters 13 places
  * @str: string
- * Return: string `s` rotated
+ * Return: str
  */
 
 char *rot13(char *str)
 {
+
 int indx1 = 0, indx2;
 char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
                      'G', 'H', 'I', 'J', 'K', 'L',
@@ -19,7 +19,6 @@ char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 		     'k', 'l', 'm', 'n', 'o', 'p',
 		     'q', 'r', 's', 't', 'u', 'v',
 		     'w', 'x', 'y', 'z'};
-
 char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 	             'T', 'U', 'V', 'W', 'X', 'Y',
 		     'Z', 'A', 'B', 'C', 'D', 'E',
@@ -29,7 +28,6 @@ char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 		     'x', 'y', 'z', 'a', 'b', 'c',
 		     'd', 'e', 'f', 'g', 'h', 'i',
 		     'j', 'k', 'l', 'm'};
-
 while (str[indx1])
 {
 for (indx2 = 0; indx2 < 52; indx2++)
