52d1d5ff24828015532d2f06e65ff65bee70ef1e
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-21 00:35:34 -0700

    task 4

---
+++ 4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in an integer
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int b;
+for (i = 0; i < n / 2; i++)
+{
+b = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = b;
+}
+}
