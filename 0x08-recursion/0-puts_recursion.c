d1145f2501efd32c6f4418bd1e20a458191c40d1
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-28 13:22:20 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion - function to print a string
+ * _puts_recursion - function
  * Return: Nothing
- * @*s: a pointer to a string
  */
 
 void _puts_recursion(char *s)
 {
+if (*s != '\0')	
+{
 putchar(*s);
-s++;
+s = s + 1;
+}
 putchar('\n');
 }
