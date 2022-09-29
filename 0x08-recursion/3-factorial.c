3b7672f495e578992a063db87acebf8cacded57a
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-29 06:25:31 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,18 +2,24 @@
 #include "main.h"
 
 /**
- * int factorial - function to calculate the factorial of a number
- * @s: string
+ * factorial - function to calculate the factorial of a number
+ * @n: interger
  * Return: factorial of a number
  */
 
 int factorial(int n)
 {
 if (n < 0)
+{
 return -1;
+}
 else if (n == 0)
+{
 return 1;
+}
 else
+{
 return (n * factorial(n - 1));
+}
 }
 
