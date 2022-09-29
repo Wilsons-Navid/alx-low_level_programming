6317dc2dfd16eebbfb406e32ad0fa86db0bb960b
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-29 10:21:11 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - function to print prime number
+ * @n: interger
+ * Return: 0 if not prime and 1 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+if (n % 2 != 0)
+return (1);
+else
+return (0);
+}
