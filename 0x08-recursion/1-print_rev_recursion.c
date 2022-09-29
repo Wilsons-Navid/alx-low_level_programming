e161ad50ce5e46a8e3860ceb7f1ba6adf493f839
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-29 00:14:38 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-if ((*s % 10) != '\0')
+if ((*s % 10) == '\0')
 {	
 putchar(*s % 10);
 _print_rev_recursion(s - 1);
 }
 else
 {
-putchar('\n');
+putchar('\n');	
 }
 }
