5957286d1fd63d54efe0b94050836ecd4ec3c731
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-29 09:22:19 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates the square root of a number
+ * @n: int
+ * Return: Natural sqrt of a number 
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (natural_root (n, 0));
+}
+
+/**
+ * natural_root - finds the natural sqrt of a number
+ * @n: int
+ * @i: counter
+ * Return: sqrt of a number
+ */
+
+int natural_root(int n, int i)
+{
+if ( i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+else 
+return (natural_root(n, i + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 char _putchar(char *s);
+int natural_root(int n, int i);
