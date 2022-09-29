815f8d39b5a484332e539525d5afc49267cdc0a8
Wilson-Navid <wadotiwawil@gmail.com>
2022-09-29 07:10:41 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - function to return x to y
+ * @x: int
+ * @y: int
+ * Return: x raised to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
