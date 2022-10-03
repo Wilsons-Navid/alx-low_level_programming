aee09fcf521f27680dc047a591c84b93f650f34b
Wilson-Navid <wadotiwawil@gmail.com>
2022-10-03 10:16:00 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - fumction to print name of the program
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);	
+}	
